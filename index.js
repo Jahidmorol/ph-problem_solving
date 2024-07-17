374c9aa275df20f833aec8f5d79f501e0fee47b4
jahid <jahidmorol2@gmail.com>
2024-07-17 10:23:20 +0600

    solved more 2 problem

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Problem 1.--=> Array Filtering and Mapping
+
 const array = [
   {
     name: "hasan",
@@ -22,9 +24,58 @@ const array = [
 ];
 
 const filterForFemale = (array) => {
-  const result = array.filter((a) => a.gender !== "female").map((a) => a.name);
+  const result = array.filter((x) => x.gender !== "female").map((y) => y.name);
 
   return result;
 };
 
-console.log(filterForFemale(array));
+// console.log(filterForFemale(array));
+
+// Problem 2.--=> Object Manipulation
+
+const books = [
+  {
+    title: "the first book",
+    author: "John",
+    year: 2000,
+  },
+  {
+    title: "the second book",
+    author: "morol",
+    year: 2001,
+  },
+  {
+    title: "the third book",
+    author: "hasan",
+    year: 2002,
+  },
+  {
+    title: "the fourth book",
+    author: "laila",
+    year: 2003,
+  },
+];
+
+const bookNewArray = (books) => {
+  return books.map((book) => book.title);
+};
+
+// console.log(bookNewArray(books));
+
+// Problem 3.--=> Function Composition
+
+const squareNum = (num) => {
+  return num * num;
+};
+const dabbleNum = (num) => {
+  return num + num;
+};
+const addNum = (num) => {
+  return num + 5;
+};
+
+const composedFunc = (num) => {
+  return addNum(dabbleNum(squareNum(num)));
+};
+
+console.log(composedFunc(3));
